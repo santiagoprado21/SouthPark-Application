2144d6bb4417bd246f0b3dd05241604a3d7b6fe8
santiagoprado21 <santiagoprado435@gmail.com>
2024-06-09 18:16:59 -0500

    Feature:App.tsx Voley - Show information of sport

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -39,10 +39,6 @@ export default function Page() {
         <div className="w-1/2 flex justify-center items-center pl-10">
           <Carousel />
         </div>
-<<<<<<< HEAD
-        <div className="pt-20 font-semibold text-sky-800 font-serif text-4xl">
-          <h1>Bienvenidos a nuestro Club</h1>
-=======
         <div className="w-1/2 flex flex-col items-center justify-center">
           <h1 className="font-semibold text-sky-800 font-serif text-4xl text-center pr-10">
             Bienvenidos a nuestro Club
@@ -50,7 +46,6 @@ export default function Page() {
           <p className="text-center font-light font-serif text-2xl pt-5">
             5 deportes en un mismo lugar
           </p>
->>>>>>> 6f4d69d9c124ed343a59f7e2783e4a9d156ddf09
         </div>
       </section>
 
